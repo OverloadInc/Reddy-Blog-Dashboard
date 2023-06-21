8f4e3c9da013d332184b7008648113c3088e38b8
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-20 23:06:30 -0600

    Add environment variable to access firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCO38IAyKvV6a2A2Aewt0RKqCw3p42ApVw",
+    authDomain: "reddy-blog.firebaseapp.com",
+    projectId: "reddy-blog",
+    storageBucket: "reddy-blog.appspot.com",
+    messagingSenderId: "1012895389647",
+    appId: "1:1012895389647:web:32ef3448da29ee7ed8e4ba"
+  }
+};
