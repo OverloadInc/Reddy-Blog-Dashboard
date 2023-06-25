1e46736c461c32192ee94b4b45d54b9ef1d6a383
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-25 01:02:46 -0600

    Update CRUD operations

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { ToastrService } from "ngx-toastr";
+import { map } from "rxjs/operators"
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,30 @@ export class CategoriesService {
     }).catch(error => {
       console.log(error);
     });
+  }
+
+  loadData() {
+    return this.angularFirestore.collection('categories').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+
+          return {id, data}
+        })
+      })
+    );
+  }
+
+  updateData(id: any, EditData: any) {
+    this.angularFirestore.doc(`categories/${id}`).update(EditData).then(docRef => {
+      this.toastr.success('Data update successfully');
+    });
+  }
+
+  deleteData(id: any) {
+    this.angularFirestore.doc(`categories/${id}`).delete().then(docRef => {
+      this.toastr.success('Data delete successfully');
+    })
   }
 }
