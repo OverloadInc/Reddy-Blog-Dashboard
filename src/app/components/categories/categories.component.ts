947cffc07eeae1d03bd1d2843838d8009ace43ea
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-24 18:47:06 -0600

    Add categories component form

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {CategoriesService} from "../../services/categories.service";
+import {Category} from "../../models/category";
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  constructor(private categoriesService: CategoriesService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(formData: any) {
+    let categoryData: Category = {
+      category: formData.value.category
+    };
+
+    this.categoriesService.saveData(categoryData);
+
+    /*let subCategoryData = {
+      subCategory: 'SubCategory 1'
+    };*/
+
+
+  }
+}
