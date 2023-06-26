8cd4270475babddc05a23a2b9931bce54d170c52
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-25 23:43:36 -0600

    Update routes with new categories, posts, and new posts paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import {Route, RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {CategoriesComponent} from "./components/categories/categories.component";
+import {AllPostsComponent} from "./components/posts/all-posts/all-posts.component";
+import {NewPostComponent} from "./components/posts/new-post/new-post.component";
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
-  {path: 'categories', component: CategoriesComponent}
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'posts', component: AllPostsComponent},
+  {path: 'posts/new', component: NewPostComponent}
 ];
 
 @NgModule({
