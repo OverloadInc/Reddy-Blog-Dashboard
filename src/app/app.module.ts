40d12367e1b10e6ee4af6a1f73df1b3c8cd4072d
Overload-Inc <overload.inc.mx@gmail.com>
2023-06-25 23:41:15 -0600

    Update NgModule imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { ToastrModule} from "ngx-toastr";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularFireModule} from "@angular/fire/compat";
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireStorageModule} from "@angular/fire/compat/storage";
+import { AngularEditorModule } from "@kolkov/angular-editor";
 import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 import { environment } from "../environments/environment.development";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { AllPostsComponent } from './components/posts/all-posts/all-posts.component';
+import { NewPostComponent } from './components/posts/new-post/new-post.component';
 
 @NgModule({
   declarations: [
@@ -19,16 +25,22 @@ import { CategoriesComponent } from './components/categories/categories.componen
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    AllPostsComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    AngularFireStorageModule,
     FormsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
