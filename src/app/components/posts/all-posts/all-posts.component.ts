172d63e81c5ef7ecf037b69d42718984ced5a184
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-01 23:02:57 -0600

    Add methods to delete and mark featured posts.

--- src/app/components/posts/all-posts/all-posts.component.ts
+++ src/app/components/posts/all-posts/all-posts.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from "../../../services/posts.service";
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.css']
 })
-export class AllPostsComponent {
+export class AllPostsComponent implements OnInit{
 
+  postArray: Array<any> = [];
+
+  constructor(private postsService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    this.postsService.loadData().subscribe(value => {
+      this.postArray = value;
+    });
+  }
+
+  onDelete(postImagePath: string, id: string) {
+    this.postsService.deleteImage(postImagePath, id);
+  }
+
+  onFeatured(id: string, value: boolean) {
+    const featuredData = {
+      isFeatured: value
+    }
+
+    this.postsService.markFeatured(id, featuredData);
+  }
 }
