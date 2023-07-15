3df1baa668a9994d434360f145c21e4ea7bf14d6
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-15 12:40:43 -0600

    Add Logout session business logic to the end the current session

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../services/auth.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  loggedUser: string = '';
+  isLoggedIn$?: Observable<boolean>;
+
+  constructor(private autService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.loggedUser = JSON.parse(localStorage.getItem('user') || '{}').email;
+
+    this.isLoggedIn$ = this.autService.isLoggedIn();
+  }
+
+  endSession() {
+    this.autService.logout();
+  }
 }
