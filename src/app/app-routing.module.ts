61e8f70201bb7573bfa79f42f2e593ac56801fac
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-15 10:59:07 -0600

    Add Login component route to the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {CategoriesComponent} from "./components/categories/categories.component";
 import {AllPostsComponent} from "./components/posts/all-posts/all-posts.component";
 import {NewPostComponent} from "./components/posts/new-post/new-post.component";
+import {LoginComponent} from "./components/auth/login/login.component";
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'categories', component: CategoriesComponent},
   {path: 'posts', component: AllPostsComponent},
   {path: 'posts/new', component: NewPostComponent}
