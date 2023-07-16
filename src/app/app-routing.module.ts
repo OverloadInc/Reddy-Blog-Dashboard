768834907451d078047f3c6d66cbf498b8185436
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-15 23:56:12 -0600

    Update AuthGuard path and component routes for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
-import {Route, RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {CategoriesComponent} from "./components/categories/categories.component";
 import {AllPostsComponent} from "./components/posts/all-posts/all-posts.component";
 import {NewPostComponent} from "./components/posts/new-post/new-post.component";
 import {LoginComponent} from "./components/auth/login/login.component";
+import {AuthGuard} from "@angular/fire/auth-guard";
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent/*, canActivate: [AuthGuard]*/},
   {path: 'login', component: LoginComponent},
-  {path: 'categories', component: CategoriesComponent},
-  {path: 'posts', component: AllPostsComponent},
-  {path: 'posts/new', component: NewPostComponent}
+  {path: 'categories', component: CategoriesComponent/*, canActivate: [AuthGuard]*/},
+  {path: 'posts', component: AllPostsComponent/*, canActivate: [AuthGuard]*/},
+  {path: 'posts/new', component: NewPostComponent/*, canActivate: [AuthGuard]*/}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {
 
