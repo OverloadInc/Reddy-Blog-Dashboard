6f5b84c9ed9ba586321ea870448072780dd71eac
Overload-Inc <overload.inc.mx@gmail.com>
2023-07-23 01:18:27 -0600

    Add subscribers path to the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import {AllPostsComponent} from "./components/posts/all-posts/all-posts.componen
 import {NewPostComponent} from "./components/posts/new-post/new-post.component";
 import {LoginComponent} from "./components/auth/login/login.component";
 import {AuthGuard} from "@angular/fire/auth-guard";
+import {SubscribersComponent} from "./components/subscribers/subscribers.component";
 
 const routes: Routes = [
   {path: '', component: DashboardComponent/*, canActivate: [AuthGuard]*/},
   {path: 'login', component: LoginComponent},
   {path: 'categories', component: CategoriesComponent/*, canActivate: [AuthGuard]*/},
   {path: 'posts', component: AllPostsComponent/*, canActivate: [AuthGuard]*/},
-  {path: 'posts/new', component: NewPostComponent/*, canActivate: [AuthGuard]*/}
+  {path: 'posts/new', component: NewPostComponent/*, canActivate: [AuthGuard]*/},
+  {path: 'subscribers', component: SubscribersComponent/*, canActivate: [AuthGuard]*/}
 ];
 
 @NgModule({
